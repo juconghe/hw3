2802b7b025998a4ce5122108524d04a3f112149b
Jucong He <juconghe@1x-nat-vl932-172-30-241-249.wireless.umass.edu>
2017-11-08 00:29:16 -0500

    add pickel

--- polarity.py
+++ polarity.py
@@ -1,15 +1,14 @@
 from collections import defaultdict
 import math
 import numpy as np
+import pickle
 
-word_count_dic = defaultdict(float)
-word_pair_count_dic = defaultdict(float)
+polarity_dic = defaultdict(float)
 pos_seed_list = ['good', 'nice', 'love',
                  'excellent', 'fortunate', 'correct', 'superior']
 neg_seed_list = ['bad', 'nasty', 'poor',
                  'hate', 'unfortunate', 'wrong', 'inferior']
 TOTAL_TWEETS = 174689.0
-polarity_dic = defaultdict(float)
 
 
 def tokenize_doc(doc):
@@ -24,7 +23,7 @@ def tokenize_doc(doc):
     return set(lowered_tokens)
 
 
-def construct_dic(doc):
+def construct_dic(doc, word_count_dic, word_pair_count_dic):
     tokens = tokenize_doc(doc)
     # print tokens
     for token in tokens:
@@ -38,7 +37,7 @@ def construct_dic(doc):
                 word_pair_count_dic[(token, n)] += 1.0
 
 
-def calculate_polarity():
+def calculate_polarity(word_count_dic, word_pair_count_dic):
     with open('tweets.txt', 'r') as doc:
         for d in doc:
             tokens = tokenize_doc(d)
@@ -64,17 +63,33 @@ def calculate_polarity():
                 polarity_dic[t] = np.mean(p_wy_pos) - np.mean(p_wy_neg)
 
 
+def write_to_pickle(word_count_dic, word_pair_count_dic):
+    with open('word_count_dic', 'w') as outfile:
+        pickle.dump(word_count_dic, outfile)
+    with open('word_pair_count_dic', 'w') as outfile:
+        pickle.dump(word_pair_count_dic, outfile)
+
+
+def load_pickle():
+    file1 = open('word_count_dic', 'rb')
+    word_count_dic = pickle.load(file1)
+    file2 = open('word_pair_count_dic', 'rb')
+    word_pair_count_dic = pickle.load(file2)
+    return word_count_dic, word_pair_count_dic
+
+
 if __name__ == '__main__':
-    print 'constructing dictionary'
-    with open('tweets.txt', 'r') as doc:
-        for d in doc:
-            construct_dic(d)
-        # for p in pos_seed_list:
-        #     print word_count_dic[p]
-        # for n in neg_seed_list:
-        #     print word_count_dic[n]
+    # word_count_dic = defaultdict(float)
+    # word_pair_count_dic = defaultdict(float)
+    # print 'constructing dictionary'
+    # with open('tweets.txt', 'r') as doc:
+    #     for d in doc:
+    #         construct_dic(d, word_count_dic, word_pair_count_dic)
+    #     print 'writing dictionaries to pickle'
+    #     write_to_pickle(word_count_dic, word_pair_count_dic)
+    word_count_dic, word_pair_count_dic = load_pickle()
     print 'calculating polarity'
-    calculate_polarity()
+    calculate_polarity(word_count_dic, word_pair_count_dic)
     print 'positive'
     for p in sorted(polarity_dic.items(), key=lambda (w, c): -c)[:50]:
         print p
