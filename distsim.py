ba2a339e4db7b10632ca086b5337486653bffc37
Jucong <hejucong1995@gmail.com>
2017-11-09 00:21:30 -0500

    part2 in progress

--- distsim.py
+++ distsim.py
@@ -1,30 +1,35 @@
 from __future__ import division
-import sys,json,math
+import sys
+import json
+import math
 import os
 import numpy as np
+
 
 def load_word2vec(filename):
     # Returns a dict containing a {word: numpy array for a dense word vector} mapping.
     # It loads everything into memory.
-    
-    w2vec={}
-    with open(filename,"r") as f_in:
+
+    w2vec = {}
+    with open(filename, "r") as f_in:
         for line in f_in:
-            line_split=line.replace("\n","").split()
-            w=line_split[0]
-            vec=np.array([float(x) for x in line_split[1:]])
-            w2vec[w]=vec
+            line_split = line.replace("\n", "").split()
+            w = line_split[0]
+            vec = np.array([float(x) for x in line_split[1:]])
+            w2vec[w] = vec
     return w2vec
+
 
 def load_contexts(filename):
     # Returns a dict containing a {word: contextcount} mapping.
     # It loads everything into memory.
 
     data = {}
-    for word,ccdict in stream_contexts(filename):
+    for word, ccdict in stream_contexts(filename):
         data[word] = ccdict
     print "file %s has contexts for %s words" % (filename, len(data))
     return data
+
 
 def stream_contexts(filename):
     # Streams through (word, countextcount) pairs.
@@ -35,3 +40,11 @@ def stream_contexts(filename):
         n = int(n)
         ccdict = json.loads(ccdict)
         yield word, ccdict
+
+
+def cosine(word_vect_1, word_vect_2):
+    dot_product = word_vect_1.dot(word_vect_2)
+    size_1 = math.sqrt(np.sum(word_vect_1 ** 2))
+    size_2 = math.sqrt(np.sum(word_vect_2 ** 2))
+    cos = dot_product / (size_1*size_2)
+    return cos
